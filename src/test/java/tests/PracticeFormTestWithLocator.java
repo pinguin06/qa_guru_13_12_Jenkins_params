04fc84f41e74be466a12b8ec7bccb690ffa9f940
Olga Romanshchak <pinguin06@rambler.ru>
2022-07-08 13:28:32 +0300

    added SystemPropertiesTests, OwnerTests

--- src/test/java/tests/PracticeFormTestWithLocator.java
+++ src/test/java/tests/PracticeFormTestWithLocator.java
@@ -1,7 +1,10 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import helpers.Attach;
+import io.qameta.allure.Owner;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -10,61 +13,53 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
+import static io.qameta.allure.Allure.step;
 
-    public class PracticeFormTestWithLocator extends TestBase {
-        @Test
-        void successfulTest() {
+public class PracticeFormTestWithLocator extends TestBase {
+
+    @Test
+    @Owner("ORomanshchak")
+    @DisplayName("Successful fill form")
+    void successfulTest() {
         String fname = "Olga";
         String lname = "Romanshchak";
         String email = "email@test.qa";
 
+        step("Открываем форму регистрации", () -> {
             open("/automation-practice-form");
-            //команды, которые прячут рекламу от гугла и нижний блок
+            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
             executeJavaScript("$('footer').remove()");
             executeJavaScript("$('#fixedban').remove()");
+            Attach.screenshotAs("Last screenshot_open");
+        });
 
-            //ВВодим значения в поля
+        step("Заполняем поля на форме", () -> {
             $("#firstName").setValue(fname);
             $("#lastName").setValue(lname);
             $("#userEmail").setValue(email);
             $("#userNumber").setValue("9291027606");
-
-            //Выбираем пол
             $("#genterWrapper").$(byText("Female")).click();
-
-            //Вводим дату рождения
             $("#dateOfBirthInput").click();
             $(".react-datepicker__month-select").click();
             $(".react-datepicker__month-select").selectOption("June");
             $(".react-datepicker__year-select").click();
             $(".react-datepicker__year-select").selectOption("1990");
             $(".react-datepicker__day--016").click();
-
-            //Выбираем предмет
             $("#subjectsInput").sendKeys("M");
             $(byText("Maths")).click();
-
-            //Выбираем хобби
             $("#hobbiesWrapper").$(byText("Sports")).click();
             $("#hobbiesWrapper").$(byText("Music")).click();
-
-            //Выбираем файл
             $("#uploadPicture").uploadFile(new File("src\\test\\resources\\1.txt"));
-
-            //Адрес
             $("#currentAddress").setValue("Saint Petersburg");
-
-            //Штат / город
             $("#state").click();
             $(byText("Haryana")).click();
-
+            Attach.screenshotAs("Last screenshot_fill");
             $("#city").click();
             $(byText("Panipat")).click();
-
-
             $("#submit").click();
+        });
 
-            //проверка выводимого результата
+        step("Проверяем результат регистрации", () -> {
             $(".modal-content").shouldHave(
                     text(fname),
                     text(lname),
@@ -77,6 +72,6 @@ import static com.codeborne.selenide.Selenide.$;
                     text("1.txt"),
                     text("Saint Petersburg"),
                     text("Haryana Panipat"));
-
-        }
+        });
     }
+}
---
+++ src/test/java/tests/properties/SystemPropertiesTests.java
@@ -0,0 +1,79 @@
+package tests.properties;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class SystemPropertiesTests {
+    @Test
+    void someTest1() {
+        String browser = System.getProperty("browser");
+        System.out.println(browser); // null
+    }
+
+    @Test
+    void someTest2() {
+        System.setProperty("browser", "safari");
+        String browser = System.getProperty("browser");
+        System.out.println(browser); // safari
+    }
+
+    @Test
+    void someTest3() {
+        String browser = System.getProperty("browser", "opera");
+        System.out.println(browser); // opera
+    }
+
+    @Test
+    void someTest4() {
+        System.setProperty("browser", "safari");
+        String browser = System.getProperty("browser", "opera");
+        System.out.println(browser); // safari
+    }
+
+    @Test
+    void someTest5() {
+        System.setProperty("anyValue", "any text");
+        String text = System.getProperty("anyValue", "some text");
+        System.out.println(text); //
+    }
+
+    @Test
+    @Tag("test6")
+    void someTest6() {
+        String browser = System.getProperty("browser", "chrome");
+        String version = System.getProperty("version", "101");
+        String browserSize = System.getProperty("browserSize", "1920x1080");
+
+        System.out.println(browser); //
+        System.out.println(version); //
+        System.out.println(browserSize); //
+
+        /*
+            From idea
+                chrome
+                101
+                1920x1080
+            gradle clean properties_test6
+                chrome
+                101
+                1920x1080
+            gradle clean properties_test6 -Dbrowser=opera
+                opera
+                101
+                1920x1080
+            gradle clean properties_test6 -Dbrowser=opera -Dversion=102
+                opera
+                102
+                1920x1080
+         */
+    }
+
+    @Test
+    @Tag("hello")
+    void someTest7() {
+        System.out.println("Hello " + System.getProperty("anyText"));
+
+        // gradle clean hello_test -DanyText=world!
+        //     Hello world!
+    }
+}
